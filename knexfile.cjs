b5505f5cd1b672cef2420b4d1bbaa209feed3759
FilipeSantos22 <filipeaprendiz@hotmail.com>
2025-08-21 10:41:55 -0300

    refatoração: reorganizar estrutura de arquivos e atualizar configurações do knex
    - Removido arquivo knexfile-old.js.
    - Atualizado knexfile.cjs para usar novos diretórios para migrações e seeds.
    - Adicionado novas dependências no package.json e atualizado versões de devDependencies.
    - Criadas novas migrações para as tabelas: usuarios, servico, barbearia, servico_barbeiro, agendamento e uma migração de teste.
    - Removido arquivo server.js.

--- knexfile.cjs
+++ knexfile.cjs
@@ -13,55 +13,28 @@ module.exports = {
     },
     pool: { min: 2, max: 10 },
     migrations: {
-      directory: path.resolve(__dirname, 'migrations'),
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
       extension: 'js',
       tableName: 'knex_migrations'
     },
     seeds: {
-      directory: path.resolve(__dirname, 'src', 'seeds'),
-      extension: 'js'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST ?? '127.0.0.1',
-      port: Number(process.env.DB_PORT ?? 5432),
-      database: process.env.DB_NAME ?? 'my_db',
-      user: process.env.DB_USER ?? 'username',
-      password: process.env.DB_PASS ?? 'password'
-    },
-    pool: { min: 2, max: 10 },
-    migrations: {
-      directory: path.resolve(__dirname, 'migrations'),
-      extension: 'js',
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: path.resolve(__dirname, 'src', 'seeds'),
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
       extension: 'js'
     }
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT ?? 5432),
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS
-    },
-    pool: { min: 2, max: 10 },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: path.resolve(__dirname, 'migrations'),
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
       extension: 'js',
       tableName: 'knex_migrations'
     },
     seeds: {
-      directory: path.resolve(__dirname, 'src', 'seeds'),
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
       extension: 'js'
-    }
+    },
+    pool: { min: 2, max: 10 }
   }
 };
