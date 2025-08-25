6c8ae21c8c5c0a0399d3e3718ede97b2d0a2bef8
FilipeSantos22 <filipeaprendiz@hotmail.com>
2025-08-25 11:35:51 -0300

    feat: adicionar coluna 'excluido' nas tabelas 'usuarios', 'servico', 'agendamento', 'barbearia' e 'servico_barbeiro'

---
+++ src/database/migrations/20250825120916_nova-coluna-tabelas.cjs
@@ -0,0 +1,44 @@
+
+exports.up = async function(knex) {
+  await knex.schema.alterTable("usuarios", (table) => {
+    table.boolean("excluido").defaultTo(false);
+  });
+
+  await knex.schema.alterTable("servico", (table) => {
+    table.boolean("excluido").defaultTo(false);
+  });
+
+  await knex.schema.alterTable("agendamento", (table) => {
+    table.boolean("excluido").defaultTo(false);
+  });
+
+  await knex.schema.alterTable("barbearia", (table) => {
+    table.boolean("excluido").defaultTo(false);
+  });
+
+  await knex.schema.alterTable("servico_barbeiro", (table) => {
+    table.boolean("excluido").defaultTo(false);
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.alterTable("usuarios", (table) => {
+    table.dropColumn("excluido");
+  });
+
+  await knex.schema.alterTable("servico", (table) => {
+    table.dropColumn("excluido");
+  });
+
+  await knex.schema.alterTable("agendamento", (table) => {
+    table.dropColumn("excluido");
+  });
+
+  await knex.schema.alterTable("barbearia", (table) => {
+    table.dropColumn("excluido");
+  });
+
+  await knex.schema.alterTable("servico_barbeiro", (table) => {
+    table.dropColumn("excluido");
+  });
+};
