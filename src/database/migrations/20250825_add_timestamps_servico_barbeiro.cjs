085d84665918d811f1ce9ff845f2475ef48672df
FilipeSantos22 <filipeaprendiz@hotmail.com>
2025-08-25 16:49:39 -0300

    feat: adicionar colunas de timestamp 'data_criacao' e 'data_atualizacao' na tabela 'servico_barbeiro'

--- src/database/migrations/20250825_add_timestamps_servico_barbeiro.cjs
+++ src/database/migrations/20250825_add_timestamps_servico_barbeiro.cjs
@@ -1,18 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
 module.exports.up = async function (knex) {
-  const exists = await knex.schema.hasTable('servico_barbearia');
+  const exists = await knex.schema.hasTable('servico_barbeiro');
   if (!exists) return;
 
-  await knex.schema.alterTable('servico_barbearia', (table) => {
+  await knex.schema.alterTable('servico_barbeiro', (table) => {
     table.timestamp('data_criacao').notNullable().defaultTo(knex.fn.now());
     table.timestamp('data_atualizacao').nullable().defaultTo(knex.fn.now());
   });
 };
 
 module.exports.down = async function (knex) {
-  const exists = await knex.schema.hasTable('servico_barbearia');
+  const exists = await knex.schema.hasTable('servico_barbeiro');
   if (!exists) return;
 
-  await knex.schema.alterTable('servico_barbearia', (table) => {
+  await knex.schema.alterTable('servico_barbeiro', (table) => {
     table.dropColumn('data_criacao');
     table.dropColumn('data_atualizacao');
   });
